b374642aaf80e23cf77f6aac248b43b4f2e9c0bc
victorsosa <victor.sosa@peopleware.do>
2016-06-25 13:04:01 -0400

    generator fixes

--- generators/app/index.js
+++ generators/app/index.js
@@ -10,8 +10,8 @@ var fs = require('fs');
 
 module.exports = yeoman.Base.extend({
   initializing: function () {
-    // Use angular-directive-boilerplate npm module as template
-    this.sourceRoot(path.join(__dirname, '../node_modules/nativescript-plugin-seed/'));
+    // Use nativescript-plugin-seed npm module as template
+    this.sourceRoot(path.join(__dirname, '../../node_modules/nativescript-plugin-seed/'));
     this.options = {
       author: {
         name: this.user.git.name(),
@@ -31,55 +31,36 @@ module.exports = yeoman.Base.extend({
       name: 'someAnswer',
       message: 'Would you like to enable this option?',
       default: true
-    }];
+    }, {
+        type: 'input',
+        name: 'name',
+        message: 'What is your plugin name?',
+        default: 'yourplugin'
+      }, {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub user name?',
+        default: 'someuser'
+      }];
 
     return this.prompt(prompts).then(function (props) {
       // To access props later use this.props.someAnswer;
       this.props = props;
-    }.bind(this));
-  },
 
-
-  // Ask name of the directive
-  askName: function () {
-    var done = this.async();
-
-    this.prompt({
-      type: 'input',
-      name: 'name',
-      message: 'What is your plugin name?',
-      default: 'yourplugin'
-    }, function (result) {
-      var name = result.name.replace(/\ /g, '-');
-
+      var name = props.name.replace(/\ /g, '-');
       this.options.name = {
         dashed: name,
         camel: camelCase(name),
         spaced: capitalize(name.replace(/\-/, '-'))
       };
 
-      done();
+      // TDOD: validate user name if it contain illegal chars
+      var username = props.username.replace(/\ /g, '');
+      this.options.author.githubId = username;
+
     }.bind(this));
   },
 
-  // Ask publisher's GitHub username
-  askGithub: function () {
-    var done = this.async();
-
-    this.prompt({
-      type: 'input',
-      name: 'username',
-      message: 'What is your GitHub user name?',
-      default: 'someuser'
-    }, function (result) {
-
-      // TDOD: validate user name if it contain illegal chars
-      var username = result.username.replace(/\ /g, '');
-
-      this.options.author.githubId = username;
-      done();
-    }.bind(this))
-  },
 
   writing: {
     dest: function () {
@@ -88,65 +69,63 @@ module.exports = yeoman.Base.extend({
       var dest = this.destinationRoot();
       var that = this;
 
-      walk(root, function (basedir, filename, stat, next) {
+      var execute = function (basedir, filename, stat, next) {
         var relativePath = basedir.replace(root, '');
         var filePath = path.join(basedir, filename);
 
-        // Ignore this repo's README
-        if (filename === 'README.md') {
-          return next();
-        };
-
         if (stat.isDirectory()) {
-
           // FIXME: if it's deep directory this won't work
           fs.mkdir(path.join(filename), next);
+          
           return;
         }
 
-        fs.readFile(filePath, function (err, stream){
+        fs.readFile(filePath, function (err, stream) {
           if (err) {
             return console.error(err);
           }
 
-          // Replace directive.md with README.md
-          if (filename === 'directive.md') {
-            filename = 'README.md';
-          }
-
           // Replace src
-          else if (filename.indexOf('directive') > -1) {
-            filename = filename.replace('directive', that.options.name.dashed);
+          else if (filename.indexOf('yourplugin') > -1) {
+            filename = filename.replace('yourplugin', that.options.name.dashed);
           }
 
           var fileString = stream.toString();
           var writeFilePath = path.join(dest, relativePath, filename);
 
           // Templating
-          fileString = fileString.replace(/the-directive/g, that.options.name.dashed);
-          fileString = fileString.replace(/theDirective/g, that.options.name.camel);
-          fileString = fileString.replace(/the.directive/g,
-            that.options.author.githubId + '.' + that.options.name.dashed);
+          fileString = fileString.replace(/nativescript-plugin-seed/g, 'nativescript-' + that.options.name.dashed);
+          fileString = fileString.replace(/[Y|y]our[P|p]lugin/g, that.options.name.camel);
+          //fileString = fileString.replace(/the.directive/g, that.options.author.githubId + '.' + that.options.name.dashed);
           fileString = fileString.replace(/nativescript-yourplugin/g, 'nativescript-' + that.options.name.dashed);
           fileString = fileString.replace(/yourplugin.android.ts/g, that.options.name.dashed + '.android.ts');
           fileString = fileString.replace(/yourplugin.common.ts/g, that.options.name.dashed + '.common.ts');
           fileString = fileString.replace(/yourplugin.ios.ts/g, that.options.name.dashed + '.ios.ts');
           fileString = fileString.replace(/yourplugin.js/g, that.options.name.dashed + '.js');
-          fileString = fileString.replace(/[t|T]he [d|D]irective/g, that.options.name.spaced);
+          fileString = fileString.replace(/[y|Y]ou [n|N]ame/g, that.options.name.spaced);
           fileString = fileString.replace(/YourName/g, that.options.author.name);
-          fileString = fileString.replace(/<mazimi@apigee.com>/g,'<' + that.options.author.email + '>');
-          fileString = fileString.replace('Angular Publishable Directive Template', '');
+          fileString = fileString.replace(/NathanWalker/g, that.options.author.githubId);
+          //fileString = fileString.replace(/<mazimi@apigee.com>/g,'<' + that.options.author.email + '>');
+          //fileString = fileString.replace('Angular Publishable Directive Template', '');
 
 
           fs.writeFile(writeFilePath, fileString, next);
         });
-      }, function(err) {
+      };
+
+      walk(root, execute, function (err) {
         console.error(err);
       });
     }
-},
+  },
 
   install: function () {
     this.installDependencies();
+  },
+
+  end: function () {
+    this.log(yosay(
+      'Happy coding... ' + chalk.green('enjoy') + ' you plugin!'
+    ));
   }
 });
